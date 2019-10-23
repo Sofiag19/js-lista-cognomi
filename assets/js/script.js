2ba2529278d512f9d687d374066c5b98de5892c8
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-23 14:24:54 +0200

    impostazioni base

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Chiedi all’utente il cognome,
+// inseriscilo in un array con altri cognomi
+// e stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova
+// QUINDI OUTPUT sarà:
+// - array disordinato di partenza
+// - array ordinato;
+// -  posizione in cui si trova il cognome dell’utente nell’array ordinato.
+
+// richiesta cognome
+var surnReq = prompt ("inserisci il tuo cognome");
+
+// array altri cognomi
+var surnList = ["Ferrari","Ricci","Rossi","Bianchi","Alunni","Giorgetti"];
+
+// array ordinato - sort
