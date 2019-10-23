e9fba8d1f198437013b86ba6b77af57fe90d1be1
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-23 15:00:28 +0200

    eliminazione console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,15 +12,12 @@ var surnReq = prompt ("inserisci il tuo cognome");
 
 // array altri cognomi
 var surnList = ["Ferrari","Ricci","Rossi","Bianchi","Alunni","Giorgetti"];
-// console.log(surnList);
 
 // aggiunta cognome
 surnList.push (surnReq);
-// console.log(surnList);
 
 // array ordinato - sort
 var surnOrd = surnList.sort ();
-// console.log(surnOrd);
 
 // OUTPUTs
 for (var i = 0; i < surnOrd.length; i++) {
